9894e1ef8dad6662dae4dad9e09bff60f1cd1657
Dougal Scott <dougal.scott@gmail.com>
2023-12-31 12:02:26 +1100

    Author Create Form

--- recipeindex/recipes/forms.py
+++ recipeindex/recipes/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from .models import Authors
 
 
 class SearchForm(forms.Form):
     text = forms.CharField()
+
+
+class AuthorCreateForm(forms.ModelForm):
+    class Meta:
+        model = Authors
+        fields = ["name"]
